d3517818f665632efa77ff7d656cf63d11066ab9
Hakandede <hakandede95@gmail.com>
2022-01-10 15:56:43 +0300

    Update tryenv.sh

--- tryenv.sh
+++ tryenv.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo -e "\033[0;32mThis file is for testing small things\033[0m"
+echo -e "\033[0;32mThis file is for testing small things\033[0m"
+echo -e "\033[0;32mThis file is for testing small things\033[0m"
+echo -e "\033[0;32mThis file is for testing small things\033[0m"
+echo -e "\033[0;32mThis file is for testing small things\033[0m"
+echo -e "\033[0;32mThis file is for testing small things\033[0m"
+exec >logfile.txt 2>&1
